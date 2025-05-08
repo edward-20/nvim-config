19f8ab264a733e4871d24c870409a6535ef25c2d
Edward Lim <ed.lim199@gmail.com>
2025-05-08 19:16:18 +1000

    debugging with dap

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -11,6 +11,7 @@ return {
         "shellcheck",
         "shfmt",
         "flake8",
+        "codelldb" -- for c++ debugging
       },
     },
     lazy = false,               -- Load during startup (eagerly)
@@ -58,6 +59,75 @@ return {
       --   filetypes = { 'html', 'css', 'javascriptreact', 'typescriptreact', 'svelte', 'vue' },
       -- })
     end
+  },
+  -- lazydev.nvim used to help with lua language server for configuration of neovim
+  {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+  },
+  -- dap
+  {
+    "mfussenegger/nvim-dap",
+    config = function()
+      -- config required
+      local dap = require("dap")
+      dap.adapters.codelldb = {
+        type = "executable",
+        command = vim.fn.stdpath("data") .. "/mason/packages/codelldb/extension/adapter/codelldb",
+      }
+    end
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim", -- closes the gap between mason.nvim and nvim-dap plugin
+    dependencies = {
+      "williamboman/mason.nvim",
+      "mfussenegger/nvim-dap"
+    },
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio"
+    },
+    event = "VeryLazy",
+    config = function()
+      require("lazydev").setup({
+        library = { "nvim-dap-ui" },
+      })
+      local dap = require("dap")
+      local dapui = require("dapui")
+
+      dapui.setup()
+
+      -- Auto open UI when debugging starts, close when it ends
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+
+      -- Key mappings
+      vim.keymap.set("n", "<F5>", dap.continue, { desc = "Start/Continue Debugger" })
+      vim.keymap.set("n", "<F10>", dap.step_over, { desc = "Step Over" })
+      vim.keymap.set("n", "<F11>", dap.step_into, { desc = "Step Into" })
+      vim.keymap.set("n", "<F12>", dap.step_out, { desc = "Step Out" })
+      vim.keymap.set("n", "<Leader>b", dap.toggle_breakpoint, { desc = "Toggle Breakpoint" })
+      vim.keymap.set("n", "<Leader>B", function()
+        dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+      end, { desc = "Set Conditional Breakpoint" })
+      vim.keymap.set("n", "<Leader>du", require("dapui").toggle, { desc = "Toggle DAP UI" })
+
+    end
+  },
+  -- task runner helpful for prelaunch tasks
+  {
+    'stevearc/overseer.nvim',
+    opts = {},
   },
   -- the colorscheme should be available when starting Neovim
   {
