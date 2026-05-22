b86a4c75ce7f1076213ff6ee50ca1c3711a50cef
Edward Lim <ed.lim199@gmail.com>
2026-05-22 11:03:56 +1000

    highlight the current file in nvimtree

--- lua/plugins/nvimTree.lua
+++ lua/plugins/nvimTree.lua
@@ -7,7 +7,12 @@ return {
       "nvim-tree/nvim-web-devicons",
     },
     config = function()
-      require("nvim-tree").setup {}
+      require("nvim-tree").setup{
+        update_focused_file = {
+          enable = true,
+          update_root = false,
+        }
+      }
     end,
   },
 }
